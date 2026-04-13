45a732fcfe4cbf7081cd63e0ad5d5b5a37421793
Sharath Sunil <alphatrinity17@gmail.com>
2026-04-13 22:39:09 +0530

    Modified DAO for getting short URL to long URL

--- main/src/main/java/com/example/demo/dao/URLDao.java
+++ main/src/main/java/com/example/demo/dao/URLDao.java
@@ -1,10 +1,16 @@
 package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.URLModel;
+
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface URLDao extends JpaRepository<URLModel, UUID> {
+
+    @Query("SELECT obj FROM URLModel obj WHERE obj.longUrl:= ")
+    Optional<URLModel> findByLongUrl(String longUrl);
 }
