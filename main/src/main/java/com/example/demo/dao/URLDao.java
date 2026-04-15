082e324e33bf9336c7211c1aa4a8ad72a55e5d0e
Sharath Sunil <alphatrinity17@gmail.com>
2026-04-15 23:22:29 +0530

    Update: modification in query for DB calls

--- main/src/main/java/com/example/demo/dao/URLDao.java
+++ main/src/main/java/com/example/demo/dao/URLDao.java
@@ -2,6 +2,7 @@ package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.URLModel;
 
@@ -11,6 +12,9 @@ import java.util.UUID;
 @Repository
 public interface URLDao extends JpaRepository<URLModel, UUID> {
 
-    @Query("SELECT obj FROM URLModel obj WHERE obj.longUrl:= ")
-    Optional<URLModel> findByLongUrl(String longUrl);
+    @Query("SELECT obj FROM URLModel obj WHERE obj.longUrl = :longUrl")
+    Optional<URLModel> findByLongUrl(@Param("longUrl") String longUrl);
+
+    @Query("SELECT obj FROM URLModel obj WHERE obj.shortUrl = :shortUrl")
+    Optional<URLModel> findByShortUrl(@Param("shortUrl")String shortUrl);
 }
